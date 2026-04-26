f037998e3e5cee5f23a2c2535e4f8709fb1b245d
Sai <sai@example.com>
2026-04-26 22:48:02 +0530

    Force add all files

--- src/pages/Report.js
+++ src/pages/Report.js
@@ -20,7 +20,7 @@ function Report() {
         ⬅ Back
       </button>
 
-      <h1>Report attendance</h1>
+      <h1>attendance report</h1>
 
       <div style={styles.box}>
         <div style={styles.header}>
